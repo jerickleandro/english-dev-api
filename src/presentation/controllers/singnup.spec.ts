5fe043b6e720161d266f75c51677e93516ebcdcf
Erick Leandro <erick@evoe.cc>
2023-06-29 16:03:49 -0300

    test: return 400 and erro if no email singup

--- src/presentation/controllers/singnup.spec.ts
+++ src/presentation/controllers/singnup.spec.ts
@@ -14,4 +14,18 @@ describe('SignUpController', () => {
         expect(httpResponse.statusCode).toBe(400);
         expect(httpResponse.body).toEqual(new Error('Missing param: name'));
     });
+
+    test('Should retunr 400 if no email is provided', () => {
+        const sut = new SignUpController();
+        const httpsRequest = {
+            body: {
+                name: 'any_name',
+                password: 'any_password',
+                passwordConfirmation: 'any_password'
+            }
+        }
+        const httpResponse = sut.handle(httpsRequest);
+        expect(httpResponse.statusCode).toBe(400);
+        expect(httpResponse.body).toEqual(new Error('Missing param: email'));
+    });
 });
