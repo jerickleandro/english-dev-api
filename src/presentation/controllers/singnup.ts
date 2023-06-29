6fc4533f41580db5d7bcdb04869a565a04a8bb97
Erick Leandro <erick@evoe.cc>
2023-06-29 16:23:54 -0300

    refactor: add validadtion in for

--- src/presentation/controllers/singnup.ts
+++ src/presentation/controllers/singnup.ts
@@ -1,14 +1,14 @@
-import { MissingParamError } from '../erros/missing-param-erro'
-import { badRequest } from '../helpers/http-helper'
-import {HttpRequest, HttpResponse} from '../protocols/http'
+import { MissingParamError } from "../erros/missing-param-erro";
+import { badRequest } from "../helpers/http-helper";
+import { HttpRequest, HttpResponse } from "../protocols/http";
 
 export class SignUpController {
-    handle (httpsRequest: HttpRequest): HttpResponse {
-        if(!httpsRequest.body.name){
-            return badRequest(new MissingParamError('name'))
-        }
-        if(!httpsRequest.body.email){
-            return badRequest(new MissingParamError('email'))
-        }
+  handle(httpsRequest: HttpRequest): HttpResponse {
+    const requiredFields = ["name", "email"];
+    for (const field of requiredFields) {
+      if (!httpsRequest.body[field]) {
+        return badRequest(new MissingParamError(field));
+      }
     }
+  }
 }
